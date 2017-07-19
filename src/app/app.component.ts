6cc0961e3930f824131065a3760e1376b56cfa4f
宋鹏 <songpeng@litingdeMacBook-Pro.local>
2017-07-19 22:10:47 +0800

    多组件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
-export class Hero {
-  id: number;
-  name: string;
-}
 
 @Component({
   styles: [`
@@ -61,21 +58,11 @@ export class Hero {
 
 <h2>My Heroes</h2>
 <ul class="heroes">
-  <li *ngFor="let hero of heroes"  (click)="onSelect(hero)">
+  <li *ngFor="let hero of heroes"  (click)="onSelect(hero)"  [class.selected]="hero === selectedHero">
      <span class="badge">{{hero.id}}</span> {{hero.name}}
   </li>
 </ul>
-
-<div *ngIf="selectedHero">
-  <h2>{{selectedHero.name}} details!</h2>
-  <div><label>id: </label>{{selectedHero.id}}</div>
-  <div><label>name: </label>{{selectedHero.name}}</div>
-  <div>
-  <label>name: </label>
-  <input [(ngModel)]="selectedHero.name" placeholder="name">
-  </div>
-  
-</div>
+<hero-detail [hero]="selectedHero"></hero-detail>
   `
 })
   export class AppComponent {
